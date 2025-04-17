57348804d1c9f1d64711592c99dda9e96c1f5df2
hao133 <36333687+hao133@users.noreply.github.com>
2025-04-17 14:50:25 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -8,7 +8,13 @@
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 git clone  https://github.com/linkease/luci-app-linkease package/linkease
 git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
-git clone -b v5-lua --single-branch --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
+
+# add luci-app-mosdns
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # 加入OpenClash核心
 #chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
